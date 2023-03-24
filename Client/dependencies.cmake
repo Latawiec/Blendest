f74f5385c8df0a25ea3c6873065b5ae73c8fb600
Latawiec <lmarzec08@gmail.com>
2023-03-25 00:19:19 +0100

    I think CMake is fixed

--- Client/dependencies.cmake
+++ Client/dependencies.cmake
@@ -1,24 +1,34 @@
+
 include(FetchContent)
 
-
-set(BOOST_VERSION_TAG boost-1.81.0)
 set(FETCHCONTENT_QUIET FALSE)
+
+# --- Fetch FTXUI --------------------------------------------------------------
+include(FetchContent)
+
+set(FETCHCONTENT_UPDATES_DISCONNECTED TRUE)
+FetchContent_Declare(ftxui
+    GIT_REPOSITORY https://github.com/ArthurSonzogni/ftxui
+    GIT_TAG v4.0.0
+)
+
+FetchContent_GetProperties(ftxui)
+if(NOT ftxui_POPULATED)
+    FetchContent_Populate(ftxui)
+    add_subdirectory(${ftxui_SOURCE_DIR} ${ftxui_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+# ------------------------------------------------------------------------------
 
 # Boost
 set(BOOST_ENABLE_CMAKE ON)
 set(BOOST_INCLUDE_LIBRARIES beast process dll asio thread chrono)
-function(BoostMakeAvailable libraryName)
-    FetchContent_Declare(boost_${libraryName}
-        GIT_REPOSITORY https://github.com/boostorg/${libraryName}.git
-        GIT_TAG ${BOOST_VERSION_TAG}
-        GIT_PROGRESS TRUE
-    )
 
-    FetchContent_MakeAvailable(boost_${libraryName})
-endfunction()
-
-# get all of boost
-BoostMakeAvailable(boost)
+FetchContent_Declare(boost
+    GIT_REPOSITORY https://github.com/boostorg/boost.git
+    GIT_TAG boost-1.81.0
+    GIT_PROGRESS TRUE
+)
+FetchContent_MakeAvailable(boost)
 # ---------
 
 # Nlohmann
@@ -28,21 +38,8 @@ FetchContent_Declare(nlohmann
     GIT_SHALLOW TRUE
     GIT_PROGRESS TRUE
 )
-
 FetchContent_MakeAvailable(nlohmann)
 # --------
-
-# FTXUI
-set(FETCHCONTENT_UPDATES_DISCONNECTED TRUE)
-FetchContent_Declare(ftxui
-    GIT_REPOSITORY https://github.com/ArthurSonzogni/ftxui
-    GIT_TAG v4.0.0
-    GIT_SHALLOW TRUE
-    GIT_PROGRESS TRUE
-)
-
-FetchContent_MakeAvailable(ftxui)
-# ----------
 
 # Nod (non-CMake)
 project(nod)
